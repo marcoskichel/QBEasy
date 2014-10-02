eb40a2d9911999b099ec37b91563eb1316c6d3b1
marcoskichel <marcos.kichel@gmail.com>
2014-10-02 18:20:50 -0300

    * Implementing integration with lazylist

--- qbe/src/main/java/br/com/boilerplate/qbe/core/QBE.java
+++ qbe/src/main/java/br/com/boilerplate/qbe/core/QBE.java
@@ -8,6 +8,8 @@ import javax.persistence.EntityManager;
 
 public class QBE {
 	private EntityManager em;
+	private QueryFactory qf;
+	private static QBE instance;
 	
 	private QBE(EntityManager em) {
 		super();
@@ -15,13 +17,23 @@ public class QBE {
 	}
 	
 	public static QBE using(EntityManager em) {
-		return new QBE(em);
+		if (instance == null || !em.equals(em))
+			instance = new QBE(em);
+		return instance;
 	}
 	
 	@SuppressWarnings("unchecked")
 	public <T> List<T> query(Exemplo exemplo) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
 		return em.createQuery(new QueryFactory(exemplo).buildQueryString()).getResultList();
 	}
+	
+	@SuppressWarnings("unchecked")
+	public <T> List<T> query(Exemplo exemplo, Integer rowsPerPage) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+		qf = new QueryFactory(exemplo);
+		return em.createQuery(qf.buildQueryString()).getResultList();
+	}
+	
+	//TODO IMPLEMENTAR NEXT
 	
 	protected static boolean isInnerPojo(Object bean) {
 		if (bean instanceof Number) 
