c61e5c10ae759ab5d61badc3cba267d1225c097e
marcoskichel <marcos.kichel@gmail.com>
2015-02-28 18:07:53 -0300

    * Start working at Unit Tests

---
+++ qbeasy/src/test/java/br/com/mk/qbeasy/model/EntidadeBase.java
@@ -0,0 +1,48 @@
+package br.com.mk.qbeasy.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@MappedSuperclass
+public abstract class EntidadeBase {
+
+	@Id
+	private Long id;
+
+	@Column
+	@Temporal(TemporalType.DATE)
+	private Date dataCadastro;
+
+	@Column
+	@Temporal(TemporalType.DATE)
+	private Date dataModificacao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public Date getDataModificacao() {
+		return dataModificacao;
+	}
+
+	public void setDataModificacao(Date dataModificacao) {
+		this.dataModificacao = dataModificacao;
+	}
+}
